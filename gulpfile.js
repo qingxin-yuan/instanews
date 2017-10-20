3f98d6ca41019103e7779b248b773106a33f4dfd
qingxin-yuan <qingxin.yn@gmail.com>
2017-10-19 19:09:52 -0700

    sass folder added, filtering article functions completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,26 @@ var gulp = require('gulp'),//load gulp
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   browserSync = require('browser-sync').create(),
-  eslint = require('gulp-eslint');
+  eslint = require('gulp-eslint'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  rename = require('gulp-rename'),
+  prettyError = require('gulp-prettyerror');
+
+//sass compiled into css files
+gulp.task('sass', function() {
+ gulp.src('./sass/style.scss')
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(autoprefixer({
+       browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
+});
 
 //run lint task first before run the script
 gulp.task('scripts',['lint'], function () {
@@ -18,7 +37,8 @@ gulp.task('lint',function () {
     .pipe(eslint.failAfterError());
 });
 gulp.task('watch', function () {
-  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('./sass/*.scss',['sass']);
+  gulp.watch('./js/*.js', ['scripts']);
 });
 // browser auto-refresh on changes
 gulp.task('browser-sync', function () {
@@ -27,7 +47,7 @@ gulp.task('browser-sync', function () {
       baseDir: "./"
     }
   })
-  gulp.watch(['*.html', 'build/js/*.js', 'css/*.css']).on('change', browserSync.reload);
+  gulp.watch(['*.html', 'build/js/*.min.js', 'build/css/*.min.css']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
 (function ($) {
-
-
-  $('select').change(function(){
-    $('ul').append('<img src="../assets/images/ajax-loader.gif" class="loading">');
-    var input = $('select option:selected').val(),url;
+  $('select').change(function () {
+    $('header').animate({
+      height: 'auto',
+      margin: '0 auto',
+      padding: 0
+    }, 500);
+    $('ul').append('<img src="./assets/images/ajax-loader.gif" class="loading">');
+    var input = $('select option:selected').val(), url;
     // console.log(input);
-    if (input !=='sections'){
-      url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
+    if (input !== 'sections') {
+      url = 'https://api.nytimes.com/svc/topstories/v2/' + input + '.json';
       url += '?' + $.param({
         'api-key': 'd26bfdbb8f424d1a87afa99e2e8989b5'
       });
       $.ajax({
         url: url,
         method: 'GET',
-      }).done(function(result) {
-     
-        // var output = '';
-        result = result.results;
+      }).done(function (result) {
         // console.log(result);
+        result = result.results;
         $('ul').empty();
-        
-        $.each(result,function(index,value){
-          if (index<=11) {
-            $('.article').append('<li><img src="'+value.multimedia[4].url+'" class="article-image"><p class="article-abstract">'+value.abstract+'</p></li>');
+        //function for sorting articles
+        var total = 12,
+            length;
+        $.each(result, function (index, value) {
+          if ((index < total) && (value.multimedia.length !== 0)) {
+            length = value.multimedia.length;
+            console.log(value.multimedia);
+            $('.article').append('<li><a href="' + value.url + '"><img src="' + value.multimedia[length-1].url + '" class="article-image"><p class="article-abstract">' + value.abstract + '</p></a></li>');
+          }else if((index < total) && (value.multimedia.length===0)){
+            total++;
           }
+          else if (index ===total) {
+            return false;
+          }
+          console.log(index);
         })
-        
-      }).fail(function(err) {
+      }).fail(function (err) {
         throw err;
-      }).always(function(){
+      }).always(function () {
         $('.loading').remove();
-
       });
-      }       
+    }
   });
-
-  })(jQuery)
+})(jQuery)
